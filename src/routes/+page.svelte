3a85ac153542e27d411558394628a85788bcdf35
j12y <jayson.delancey@gmail.com>
2023-09-28 19:38:32 -0700

    github pages configuration

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,4 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>DeveloperXP</h1>
+<p>Developer experience observations for learning new APIs and SDKs.</p>
+
+
