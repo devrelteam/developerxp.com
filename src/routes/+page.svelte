c66578f22f98b196018252d391c7153e19688975
j12y <jayson.delancey@gmail.com>
2023-10-06 11:11:11 -0700

    schedule updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,12 @@
 <h1>DeveloperXP</h1>
-<p>Developer experience observations for learning new APIs and SDKs.</p>
+<p>Developer experience observations when learning new developer tools, APIs, and SDKs.</p>
 
+<h2>Schedule</h2>
+<p>Shows at 3:30pm Pacific time</p>
+
+<ul>
+<li>Oct 6: Bito.ai</li>
+<li>Oct 7: Square APIs</li>
+<li>Oct 9: ReplIT Ghostwriter</li>
+</ul>
 
