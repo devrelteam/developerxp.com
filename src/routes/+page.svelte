665d6d1818c4479e6e0dfe3e906d90367be0f785
j12y <jayson.delancey@gmail.com>
2023-12-19 15:58:33 -0800

    schedule updates

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,9 +4,9 @@
 </svelte:head>
 
 
+
 <h1>DeveloperXP</h1>
-<p>Developer Experience is the art and science of creating tools for developers. Stay tuned for </p>
-<p>Developer experience observations when learning new developer tools, APIs, and SDKs.</p>
+<p>Developer Experience is the art and science of creating tools for developers.</p>
 
 <h2>Let's Go | Show Schedule</h2>
 <p>Shows at 3:30pm (Pacific Time)</p>
@@ -15,11 +15,10 @@
 
 <p>Let's go try:</p>
 <ul>
-<li>Oct 10: ReplIT Ghostwriter</li>
-<li>Oct 12: Google Vertex</li>
-<li>Oct 16: Atlassian Forge</li>
-<li>Oct 20: Monite</li>
-<li>To be announced: Ably, Clarifai, Okta</li>
+<li>Dec 20: Contentful</li>
+<li>Dec 22: Tabnine</li>
+<li>Dec 27: Supabase</li>
+<li>Dec 29: Growthbook</li>
 </ul>
 
-<p>If you are interested in receiving a full report of the developer experience for an API product, SaaS or library -- reach out to <a href="mailto:devex@developerxp.com">devex@developerxp.com</a> to learn more.</p>
+<p>If you are interested in receiving a full report of the developer experience for an API product, SaaS tool or library -- reach out to <a href="mailto:devex@developerxp.com">devex@developerxp.com</a> to learn more.</p>
